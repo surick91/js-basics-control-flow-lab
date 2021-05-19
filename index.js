ce5f4b5a8cf54adfd064dbda35ae64a9975adc86
sam <surickj@gmail.com>
2021-05-19 10:41:41 -0700

    commit tree 2e4b3f4c3c392fecab706851027eb282f38c10d0

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
-}
+if (scuberGreetingForFeet <= 400){
+   console.log('This one is on me!');
+
+} else if (someNumber >=2000 &&someNumber <2500)
+  return 'I will gladly take your thirty bucks.'
+  } 
+
 
 function ternaryCheckCity(){
   // Write your code here!
